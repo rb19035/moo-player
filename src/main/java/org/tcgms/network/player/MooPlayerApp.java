862477fc5ab745b87bc458c37796e388002b792a
rb19035 <24275589+rb19035@users.noreply.github.com>
2022-01-23 20:55:34 -0500

    Added Spring Boot integration

---
+++ src/main/java/org/tcgms/network/player/MooPlayerApp.java
@@ -0,0 +1,15 @@
+package org.tcgms.network.player;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooPlayerApp
+{
+    public static void main( String [] args )
+    {
+        SpringApplication.run( MooPlayerApp.class, args);
+    }
+}
+
