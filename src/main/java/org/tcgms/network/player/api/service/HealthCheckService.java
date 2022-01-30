05cff894833f63ad09ffe5775178bafb1b4272ce
rb19035 <24275589+rb19035@users.noreply.github.com>
2022-01-30 13:32:55 -0500

    Added comments to MooPlayerHealthCheckAPI and HealthCheckService

--- src/main/java/org/tcgms/network/player/api/service/HealthCheckService.java
+++ src/main/java/org/tcgms/network/player/api/service/HealthCheckService.java
@@ -10,18 +10,31 @@ import org.tcgms.network.player.exception.MooPlayerException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 
+/**
+ * Class encapsulates logic for a simple heath check for the application
+ */
 @Service
 public class HealthCheckService
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( HealthCheckService.class );
 
+    /**
+     * Method executes a simple application health check and returns a DTO object
+     *
+     * @return HealthCheckDTO
+     * @throws MooPlayerException
+     */
     public HealthCheckDTO executeHealthCheck() throws MooPlayerException
     {
         HealthCheckDTO healthCheckDTO = new HealthCheckDTO();
 
+        // Get a reference to the underlying OS
         OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+
+        // Get the current system load
         healthCheckDTO.setSystemLoad( operatingSystemMXBean.getSystemLoadAverage() );
 
+        // TO-DO ... Not sure if this is needed
         healthCheckDTO.setDatabaseStatus( MooPlayerSystemStatus.OK );
 
         return healthCheckDTO;
